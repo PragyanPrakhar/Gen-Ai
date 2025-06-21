7fe75a4fbb19b576f054cf24fdffd23ffc2fe818
Pragyan Prakhar <pragyanprakhar@gmail.com>
2025-06-21 12:06:10 +0530

    Implemented result polling

--- 06-RAG-QUEUE/server.py
+++ 06-RAG-QUEUE/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Query
+from fastapi import FastAPI,Query,Path
 from job_queue.connection import queue
 from job_queue.worker import process_query
 # from job_queue.tasks import enqueue_query_job
@@ -22,3 +22,12 @@ def chat(
 
     # User ko bolo your job received
     return {"status": "queued", "job_id": job.id}
+
+
+@app.get("/result/{job_id}")
+def get_result(
+    job_id:str=Path(...,description="Job ID")
+):
+    job=queue.fetch_job(job_id=job_id);
+    result=job.return_value();
+    return {"result":result}
